bdf2b4d8d405b3967da06df1e92717e7df8277fd
George Michokostas <michokostas@gmail.com>
2017-09-11 11:35:07 +0200

    add command line argument support for rows and columns of the grid, defaults to (10x10)

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
+var (
+	rows    = flag.Int("rows", 10, "rows of the grid")
+	columns = flag.Int("columns", 10, "columns of the grid")
+)
+
 func main() {
-	grid, err := NewGrid(4, 4)
+	flag.Parse()
+
+	grid, err := NewGrid(*rows, *columns)
 	if err != nil {
 		fmt.Printf("Error create new grid: %v", err)
 	}
